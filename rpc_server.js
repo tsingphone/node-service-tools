7250579885a8a166384f696c1398ab0ba0dfdbae
Administrator <tsingphone@163.com>
2017-08-07 13:24:03 +0800

    rpc

--- rpc_server.js
+++ rpc_server.js
@@ -10,6 +10,37 @@ let options = {
 let server = net.createServer(options);
 
 server.on('connection',function (sock) {
+    log('server 已建立连接');
+    log(sock);
+
+    sock.on('connect',function (data) {
+
+    })
+
+    sock.on('data',function (data) {
+
+    })
+
+    sock.on('clsoe',function (data) {
+
+    })
+
+    sock.on('end',function (data) {
+
+    })
+
+    sock.on('error',function (data) {
+
+    })
+
+    sock.on('drain',function (data) {
+
+    })
+
+    sock.on('timeout',function (data) {
+
+    })
+
 
 });
 
